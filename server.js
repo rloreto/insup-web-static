76eb852fd66634e0b1ca3a26c957194dac312924
roberto <rloreto@gmail.com>
2018-07-25 21:02:33 +0200

    change

--- server.js
+++ server.js
@@ -6,4 +6,5 @@ require('http').createServer(function (request, response) {
   request.addListener('end', function () {
     fileServer.serve(request, response);
   }).resume();
+
 }).listen(process.env.NODE_PORT || 80);
