544ec8738a679909c4e00374f374e61210fc4762
roberto <rloreto@gmail.com>
2018-07-25 21:17:42 +0200

    c2

--- server.js
+++ server.js
@@ -7,4 +7,4 @@ require('http').createServer(function (request, response) {
   request.addListener('end', function () {
     fileServer.serve(request, response);
   }).resume();
-}).listen(process.env.NODE_PORT || 80);
+}).listen(process.env.PORT || 3000);
